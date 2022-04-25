ef0e44b9be9becca2c9cac3cf52174c055cb6215
daniil-k <daniil.kaznacheev@hilltopads.com>
2022-04-25 14:47:39 +0300

    fix express.js and add in package.json fix for routing

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const PORT = 3000;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res){
-    res.render(`${__dirname}/dist/index.html`);
+app.get('*', function(req, res){
+    res.sendfile(__dirname + '/dist/index.html');
 });
-
 
 app.listen(PORT, () => {
     console.log(`Мой текст и порт: ${PORT}!`);
