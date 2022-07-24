cadb1076ab0b00e89e05dd08c77fd4181a2b85f5
daniil-k <daniil.kaznacheev@hilltopads.com>
2022-07-24 14:50:40 +0300

    Add router to project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './style.less';
-import render from './utils/renderDOM';
+import Router from './utils/router';
 import Page404 from './pages/404';
 import Page500 from './pages/500';
 import PageLogin from './pages/login';
@@ -9,27 +9,15 @@ import PageProfile from './pages/profile';
 import pageChangePassword from './pages/profile/change-pwd';
 import PageEditProfile from './pages/profile/edit';
 
-if (window.location.pathname === '/404') {
-  render('.app', Page404);
-}
-if (window.location.pathname === '/500') {
-  render('.app', Page500);
-}
-if (window.location.pathname === '/') {
-  render('.app', PageLogin);
-}
-if (window.location.pathname === '/reg') {
-  render('.app', PageReg);
-}
-if (window.location.pathname === '/chats') {
-  render('.app', PageChats);
-}
-if (window.location.pathname === '/profile') {
-  render('.app', PageProfile);
-}
-if (window.location.pathname === '/profile/edit') {
-  render('.app', PageEditProfile);
-}
-if (window.location.pathname === '/profile/change-pwd') {
-  render('.app', pageChangePassword);
-}
+const router = new Router('.app');
+
+router
+  .use('/', PageLogin)
+  .use('/500', Page500)
+  .use('/404', Page404)
+  .use('/reg', PageReg)
+  .use('/chats', PageChats)
+  .use('/profile', PageProfile)
+  .use('/profile/edit', PageEditProfile)
+  .use('/profile/change-pwd', pageChangePassword)
+  .start();
