c9ab9714ab3101f0dc0eaef21af2015a57c184c5
daniil-k <daniil.kaznacheev@hilltopads.com>
2022-08-16 20:04:59 +0300

    Update Node to v16.15.1 and rebuild project and deploy

--- src/utils/router.spec.ts
+++ src/utils/router.spec.ts
@@ -1,20 +1,43 @@
+import { JSDOM } from 'jsdom';
 import { expect } from 'chai';
+import Block from './block';
 import Router from './router';
-import PageLogin from '../pages/login';
 
-const router = new Router('.app');
+class testPage extends Block {
+  constructor() {
+    super('div');
+  }
+
+  render() {
+    return this.compile('<h1>Page</h1>', {});
+  }
+}
 
 describe('Router test', () => {
+  beforeEach(() => {
+    const { window } = new JSDOM(
+      `<html>
+         <body>
+          <div id="root"></div>
+         </body>
+       </html>`,
+      { url: 'http://localhost' },
+    );
+    global.window = window;
+    global.document = window.document;
+  });
+
   it('check use function.', () => {
-    router.use('/login', PageLogin);
-    expect(router.routes).to.eq(1);
+    const expTestPage = new testPage();
+    const router = new Router('.app');
+    router.use('/', expTestPage);
+    expect(router.routes.length).to.eq(1);
   });
 
-  it('check go function', () => {
-    router.go('');
-  });
-
-  it('check getRoute function', () => {
-
+  it('check use function.', () => {
+    const expTestPage = new testPage();
+    const router = new Router('.app');
+    router.use('/', expTestPage);
+    expect(router.routes.length).to.eq(1);
   });
 });
