9dc00c019bcea82b1021e61fb18c5fb8af8fff40
daniil-k <daniil.kaznacheev@hilltopads.com>
2022-08-16 10:07:11 +0300

    Add docker and deploy to Heroku (refactor)

--- heroku-server.js
+++ heroku-server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/dist/index.html');
 });
 
 app.listen(process.env.PORT, () => {
